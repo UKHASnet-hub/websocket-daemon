f526908ecd7d5f9eb7bd1c9d9db01189dc247cf5
Phil Crump <phildcrump@gmail.com>
2014-05-18 12:26:13 +0100

    Add source file.

---
+++ websocket-daemon.js
@@ -0,0 +1,63 @@
+var express = require('express')
+var app = express()
+var http = require('http')
+var server = http.createServer(app)
+var io = require('socket.io').listen(server)
+
+var pg = require('pg')
+var dbConfig = require('./config.json')
+
+server.listen(3000)
+console.log('ukhas.net websocket v0.3 running...')
+
+// /socket.io
+io.disable('browser client'); // Don't serve js file (www/static/ does this)
+io.set('log level', 1); // reduce logging
+io.set('transports', [
+    'websocket'
+  , 'htmlfile'
+  , 'xhr-polling'
+  , 'jsonp-polling'
+]);
+
+// Websocket connection used for the logtail web page
+var logtailRoom = io.of('/logtail').on('connection', function (socket) {
+    var startTime = new Date();
+    pg.connect(dbConfig, function(err, client, done) {
+        if(err) {
+            res.send('Database Connection Error')
+            console.log('DB Connection Error: ', err)
+            return
+        }
+        client.query('SELECT upload.id AS i,upload.nodeid as ni,nodes.name as nn,upload.time as t,upload.packet as p,upload.state as s,upload.rssi as r FROM ukhasnet.upload INNER JOIN ukhasnet.nodes ON nodes.id=upload.nodeid ORDER BY upload.time DESC LIMIT 200;', function(err, result) {
+            done()
+            if(err) {
+                res.send('Database Query Error')
+                console.log('DB Query Error: ', err)
+                return
+            }
+            socket.emit("logtail_l200", JSON.stringify(result.rows));
+            console.log("WS: logtail_l200 served in",new Date() - startTime,"ms")
+        });
+    });
+});
+
+// Postgres Notifications
+pg.connect(dbConfig, function(err, client) {
+    if(err) {
+        console.log('DB Connection Error: ',err);
+    }
+    client.on('notification', function(msg) {
+        logtailRoom.emit("upload_row",msg.payload)
+    });
+    var query = client.query("LISTEN upload_row");
+});
+pg.connect(dbConfig, function(err, client) {
+    if(err) {
+        console.log('DB Connection Error: ',err);
+    }
+    client.on('notification', function(msg) {
+        logtailRoom.emit("upload_parse",msg.payload)
+    });
+    var query = client.query("LISTEN upload_parse");
+});
